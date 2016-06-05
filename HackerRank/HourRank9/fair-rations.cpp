93316c1411286d7cd2cb270ce2966c2bfd0a6ebb
Juan Torres <juanto121@hotmail.com>
2016-06-05 01:55:34 -0500

    Hacker rank 9

---
+++ HackerRank/HourRank9/fair-rations.cpp
@@ -0,0 +1,50 @@
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <ctime>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <string>
+#include <bitset>
+#include <cstdio>
+#include <limits>
+#include <vector>
+#include <climits>
+#include <cstring>
+#include <cstdlib>
+#include <fstream>
+#include <numeric>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+
+int main(){
+    int N;
+    cin >> N;
+    vector<char> B(N);
+    int odd = 0;
+    int even = 0;
+    bool in = false;
+    
+    for(int B_i = 0;B_i < N;B_i++){
+       	cin >> B[B_i];
+	    if( (B[B_i]-'0') %2 != 0){
+	        odd++;
+	        in = !in;
+	    }
+	    if(in)even++;
+    }
+
+    if(odd%2!=0){
+        cout << "NO";
+    }else{
+        cout << even*2;
+    }
+	
+    return 0;
+}
